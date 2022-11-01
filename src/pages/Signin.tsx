15c4cb45fa9d1be284c821281e955ea9e5296874
Ikhyun Kim <fragih@gmail.com>
2022-11-01 14:14:42 +0900

    Test google login

---
+++ src/pages/Signin.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import GoogleLogin from "react-google-login";
+
+export default function Signin() {
+  const responseGoogle = (res: any) => {
+    console.log(res);
+  };
+
+  return (
+    <GoogleLogin
+      clientId={process.env.REACT_APP_CLIENT_ID as string}
+      buttonText="구글로 계속하기"
+      onSuccess={responseGoogle}
+      onFailure={responseGoogle}
+    />
+  );
+}
+
